0d1df5bee6c9d9cbea8ad07d5d92cb6ab7f8b2bd
Derk Norton <derk.norton@gmail.com>
2024-11-30 12:43:29 -0700

    Renamed prefixes for imports.

--- v2/Module_test.go
+++ v2/Module_test.go
@@ -13,7 +13,7 @@
 package module_test
 
 import (
-	mod "github.com/craterdog/go-missing-utilities/v2"
+	uti "github.com/craterdog/go-missing-utilities/v2"
 	ass "github.com/stretchr/testify/assert"
 	tes "testing"
 )
@@ -50,49 +50,49 @@ const reserved = `
 
 func TestStringManipulation(t *tes.T) {
 	var mixedCase = "helloWorld"
-	var lowerCase = mod.MakeLowerCase(mixedCase)
+	var lowerCase = uti.MakeLowerCase(mixedCase)
 	ass.Equal(t, "helloWorld", lowerCase)
 
-	var snakeCase = mod.MakeSnakeCase(mixedCase)
+	var snakeCase = uti.MakeSnakeCase(mixedCase)
 	ass.Equal(t, "hello-world", snakeCase)
 
-	var upperCase = mod.MakeUpperCase(mixedCase)
+	var upperCase = uti.MakeUpperCase(mixedCase)
 	ass.Equal(t, "HelloWorld", upperCase)
 
-	var allCaps = mod.MakeAllCaps(mixedCase)
+	var allCaps = uti.MakeAllCaps(mixedCase)
 	ass.Equal(t, "HELLO_WORLD", allCaps)
 
-	var plural = mod.MakePlural(mixedCase)
+	var plural = uti.MakePlural(mixedCase)
 	ass.Equal(t, "helloWorlds", plural)
 
 	mixedCase = "HelloWorld"
-	lowerCase = mod.MakeLowerCase(mixedCase)
+	lowerCase = uti.MakeLowerCase(mixedCase)
 	ass.Equal(t, "helloWorld", lowerCase)
 
-	snakeCase = mod.MakeSnakeCase(mixedCase)
+	snakeCase = uti.MakeSnakeCase(mixedCase)
 	ass.Equal(t, "hello-world", snakeCase)
 
-	upperCase = mod.MakeUpperCase(mixedCase)
+	upperCase = uti.MakeUpperCase(mixedCase)
 	ass.Equal(t, "HelloWorld", upperCase)
 
-	allCaps = mod.MakeAllCaps(mixedCase)
+	allCaps = uti.MakeAllCaps(mixedCase)
 	ass.Equal(t, "HELLO_WORLD", allCaps)
 
-	plural = mod.MakePlural(mixedCase)
+	plural = uti.MakePlural(mixedCase)
 	ass.Equal(t, "HelloWorlds", plural)
 
 	var singular = "mess"
-	plural = mod.MakePlural(singular)
+	plural = uti.MakePlural(singular)
 	ass.Equal(t, "messes", plural)
 
 	plural = "pixies"
-	var unchanged = mod.MakePlural(plural)
+	var unchanged = uti.MakePlural(plural)
 	ass.Equal(t, plural, unchanged)
 
-	var actual = mod.ReplaceAll(template, "mixedName", "mixedValue")
+	var actual = uti.ReplaceAll(template, "mixedName", "mixedValue")
 	ass.Equal(t, expected, actual)
 
-	actual = mod.ReplaceAll(template, "MixedName", "string")
+	actual = uti.ReplaceAll(template, "MixedName", "string")
 	ass.Equal(t, reserved, actual)
 }
 
@@ -108,55 +108,55 @@ func (v *Class) DoNothing() {
 
 func TestObjectReflection(t *tes.T) {
 	var emptyString string
-	ass.True(t, mod.IsUndefined(emptyString))
-	ass.False(t, mod.IsDefined(emptyString))
+	ass.True(t, uti.IsUndefined(emptyString))
+	ass.False(t, uti.IsDefined(emptyString))
 
 	var hello = "Hello World"
-	ass.False(t, mod.IsUndefined(hello))
-	ass.True(t, mod.IsDefined(hello))
+	ass.False(t, uti.IsUndefined(hello))
+	ass.True(t, uti.IsDefined(hello))
 
 	var array []int
-	ass.True(t, mod.IsUndefined(array))
-	ass.False(t, mod.IsDefined(array))
+	ass.True(t, uti.IsUndefined(array))
+	ass.False(t, uti.IsDefined(array))
 
 	array = []int{1, 2, 3}
-	ass.False(t, mod.IsUndefined(array))
-	ass.True(t, mod.IsDefined(array))
+	ass.False(t, uti.IsUndefined(array))
+	ass.True(t, uti.IsDefined(array))
 
 	array = array[1:]
-	ass.False(t, mod.IsUndefined(array))
-	ass.True(t, mod.IsDefined(array))
+	ass.False(t, uti.IsUndefined(array))
+	ass.True(t, uti.IsDefined(array))
 
 	var m map[string]int
-	ass.True(t, mod.IsUndefined(m))
-	ass.False(t, mod.IsDefined(m))
+	ass.True(t, uti.IsUndefined(m))
+	ass.False(t, uti.IsDefined(m))
 
 	m = map[string]int{
 		"hello": 1,
 		"world": 2,
 	}
-	ass.False(t, mod.IsUndefined(m))
-	ass.True(t, mod.IsDefined(m))
+	ass.False(t, uti.IsUndefined(m))
+	ass.True(t, uti.IsDefined(m))
 
 	var anything any
-	ass.True(t, mod.IsUndefined(anything))
-	ass.False(t, mod.IsDefined(anything))
+	ass.True(t, uti.IsUndefined(anything))
+	ass.False(t, uti.IsDefined(anything))
 
 	anything = 5
-	ass.False(t, mod.IsUndefined(anything))
-	ass.True(t, mod.IsDefined(anything))
+	ass.False(t, uti.IsUndefined(anything))
+	ass.True(t, uti.IsDefined(anything))
 
 	var nilPointer Interface
-	ass.True(t, mod.IsUndefined(nilPointer))
-	ass.False(t, mod.IsDefined(nilPointer))
+	ass.True(t, uti.IsUndefined(nilPointer))
+	ass.False(t, uti.IsDefined(nilPointer))
 
 	nilPointer = nil
-	ass.True(t, mod.IsUndefined(nilPointer))
-	ass.False(t, mod.IsDefined(nilPointer))
+	ass.True(t, uti.IsUndefined(nilPointer))
+	ass.False(t, uti.IsDefined(nilPointer))
 
 	var pointer Interface = &Class{}
-	ass.False(t, mod.IsUndefined(pointer))
-	ass.True(t, mod.IsDefined(pointer))
-	ass.True(t, mod.ImplementsType(pointer, (*Interface)(nil)))
-	ass.False(t, mod.ImplementsType(anything, (*Interface)(nil)))
+	ass.False(t, uti.IsUndefined(pointer))
+	ass.True(t, uti.IsDefined(pointer))
+	ass.True(t, uti.ImplementsType(pointer, (*Interface)(nil)))
+	ass.False(t, uti.ImplementsType(anything, (*Interface)(nil)))
 }
