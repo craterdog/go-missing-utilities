b25d30188e0a0a93e28c5cb88481dad63e9707a3
Derk Norton <derk.norton@gmail.com>
2025-06-03 16:47:42 -0600

    Added codex and random related functions.

--- v7/module_test.go
+++ v7/module_test.go
@@ -465,3 +465,174 @@ func TestReflection(t *tes.T) {
 	ass.True(t, uti.ImplementsInterface(pointer, target))
 	ass.False(t, uti.ImplementsInterface(anything, target))
 }
+
+func TestBase16EmptyRoundTrip(t *tes.T) {
+	var bytes = make([]byte, 0)
+
+	// Encode as base 16.
+	var base16 = uti.Base16Encode(bytes)
+
+	// Decode base 16 to bytes.
+	var decoded = uti.Base16Decode(base16)
+	ass.Equal(t, bytes, decoded)
+
+	// Encode as base 16 again.
+	var encoded = uti.Base16Encode(decoded)
+	ass.Equal(t, base16, encoded)
+
+	// Decode base 16 again.
+	var again = uti.Base16Decode(encoded)
+	ass.Equal(t, again, decoded)
+}
+
+func TestBase16RoundTrip(t *tes.T) {
+	// Seed the bytes.
+	var bytes = make([]byte, 256)
+	for index := range bytes {
+		bytes[index] = byte(index)
+	}
+
+	for index := 0; index < len(bytes); index++ {
+		// Encode as base 16.
+		var base16 = uti.Base16Encode(bytes[:index+1])
+
+		// Decode base 16 to bytes.
+		var decoded = uti.Base16Decode(base16)
+		ass.Equal(t, bytes[:index+1], decoded)
+
+		// Encode as base 16 again.
+		var encoded = uti.Base16Encode(decoded)
+		ass.Equal(t, base16, encoded)
+
+		// Decode base 16 again.
+		var again = uti.Base16Decode(encoded)
+		ass.Equal(t, again, decoded)
+	}
+}
+
+func TestBase32EmptyRoundTrip(t *tes.T) {
+	var bytes = make([]byte, 0)
+
+	// Encode as base 32.
+	var base32 = uti.Base32Encode(bytes)
+
+	// Decode base 32 to bytes.
+	var decoded = uti.Base32Decode(base32)
+	ass.Equal(t, bytes, decoded)
+
+	// Encode as base 32 again.
+	var encoded = uti.Base32Encode(decoded)
+	ass.Equal(t, base32, encoded)
+
+	// Decode base 32 again.
+	var again = uti.Base32Decode(encoded)
+	ass.Equal(t, again, decoded)
+}
+
+func TestBase32RoundTrip(t *tes.T) {
+	// Seed the bytes.
+	var bytes = make([]byte, 256)
+	for index := range bytes {
+		bytes[index] = byte(index)
+	}
+
+	for index := 0; index < len(bytes); index++ {
+		// Encode as base 32.
+		var base32 = uti.Base32Encode(bytes[:index+1])
+
+		// Decode base 32 to bytes.
+		var decoded = uti.Base32Decode(base32)
+		ass.Equal(t, bytes[:index+1], decoded)
+
+		// Encode as base 32 again.
+		var encoded = uti.Base32Encode(decoded)
+		ass.Equal(t, base32, encoded)
+
+		// Decode base 32 again.
+		var again = uti.Base32Decode(encoded)
+		ass.Equal(t, again, decoded)
+	}
+}
+
+func TestBase64EmptyRoundTrip(t *tes.T) {
+	var bytes = make([]byte, 0)
+
+	// Encode as base 64.
+	var base64 = uti.Base64Encode(bytes)
+
+	// Decode base 64 to bytes.
+	var decoded = uti.Base64Decode(base64)
+	ass.Equal(t, bytes, decoded)
+
+	// Encode as base 64 again.
+	var encoded = uti.Base64Encode(decoded)
+	ass.Equal(t, base64, encoded)
+
+	// Decode base 64 again.
+	var again = uti.Base64Decode(encoded)
+	ass.Equal(t, again, decoded)
+}
+
+func TestBase64RoundTrip(t *tes.T) {
+	// Seed the bytes.
+	var bytes = make([]byte, 240)
+	for index := range bytes {
+		bytes[index] = byte(index)
+	}
+
+	for index := 0; index < len(bytes); index++ {
+		// Encode as base 64.
+		var base64 = uti.Base64Encode(bytes[:index+1])
+
+		// Decode base 64 to bytes.
+		var decoded = uti.Base64Decode(base64)
+		ass.Equal(t, bytes[:index+1], decoded)
+
+		// Encode as base 64 again.
+		var encoded = uti.Base64Encode(decoded)
+		ass.Equal(t, base64, encoded)
+
+		// Decode base 64 again.
+		var again = uti.Base64Decode(encoded)
+		ass.Equal(t, again, decoded)
+	}
+}
+
+func TestRandomBooleans(t *tes.T) {
+	var foundFalse uint
+	var foundTrue uint
+	for i := 0; i < 100; i++ {
+		if uti.RandomBoolean() {
+			foundTrue++
+		} else {
+			foundFalse++
+		}
+	}
+	ass.True(t, foundFalse > 40)
+	ass.True(t, foundTrue > 40)
+}
+
+func TestRandomOrdinals(t *tes.T) {
+	var foundZero bool
+	var foundFive bool
+	for i := 0; i < 100; i++ {
+		var random = uti.RandomOrdinal(5)
+		if random == 0 {
+			foundZero = true
+		}
+		if random == 5 {
+			foundFive = true
+		}
+	}
+	ass.False(t, foundZero)
+	ass.True(t, foundFive)
+}
+
+func TestRandomProbabilities(t *tes.T) {
+	var total uti.Probability
+	for i := 0; i < 10000; i++ {
+		total += uti.RandomProbability()
+	}
+	ass.True(t, total > 4800)
+	ass.True(t, total < 5200)
+}
